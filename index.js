48f4b057be64b10501dca992e65d140146d6dcf3
Astha Singh <95242101+Prolifictalen@users.noreply.github.com>
2024-12-07 21:19:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./css/index.css";
+import express from "express";
+import cors from "cors";
+import dotenv from 'dotenv';
+import UserRoutes from "./routes/User/UserRoutes.js";
+import AdminRoutes from "./routes/Admin/AdminRoutes.js";
+import ConnectDB from './config/ConnectDB.js'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-    <App />
-  </>
-);
+const app=express();
+app.use(cors());
+app.use(express.json());
 
+dotenv.config()
+ConnectDB()
+
+const port = process.env.port || 5000
+
+app.use("/api/v1/admin/",AdminRoutes)
+app.use("/api/v1/user/",UserRoutes)
+
+app.listen(port,()=>console.log("Connected"));
